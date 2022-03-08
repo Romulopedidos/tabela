5792e0c2f55e6f35e0c6ab2394e0b141c1b7f468
Romulopedidos <98947494+Romulopedidos@users.noreply.github.com>
2022-03-08 16:30:37 -0300

    Gerakey
    função gerakey e parametro adcionado que permite acessar a função

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import * as s from './Styled'
 
 export default function TabelaItens() {
+
+
   let dadosSalvo = localStorage.getItem('peca');
   console.log(dadosSalvo)
 
-  const [peca, setPeca] = useState(JSON.parse(dadosSalvo))
+  const [peca, setPeca] = useState([])
+
+  useEffect(()=>{
+    setPeca(JSON.parse(dadosSalvo))
+  },[])
 
 
 
-  function excluir() {
-    localStorage.removeItem('peca')
-    setPeca([])
-
-
-  }
 
   function armazenar() {
+
 
     let npeca = document.getElementById("npeca").value;
     let npreco = document.getElementById("npreco").value;
@@ -25,11 +26,12 @@ export default function TabelaItens() {
     let pp = document.getElementById("pp").value;
     let credor = document.getElementById("credor").value;
 
+  
 
     let arrauObjetos = [];
-
     let dados = {
-
+      
+      key: gerakey(),
       npeca: npeca,
       npreco: npreco,
       qtd: qtd,
@@ -37,15 +39,37 @@ export default function TabelaItens() {
       credor: credor,
     }
 
-    arrauObjetos.push(...peca, dados)
+
+    arrauObjetos.push( ...peca, dados)
+
+    console.log(arrauObjetos)
 
     localStorage.setItem("peca", JSON.stringify(arrauObjetos));
+
     setPeca(arrauObjetos)
 
 
   }
 
+
+  function gerakey(){
+      let min = Math.ceil(0);
+     let max = Math.floor(1000);
+      
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+  
+    console.log(peca)
+
+
+    
+  function excluir() {
+    setPeca([])
+    
+
+  }
   return (
+
 
 
 
@@ -57,16 +81,16 @@ export default function TabelaItens() {
       <s.Inovos id="npeca"   ></s.Inovos>
 
       <s.Etiqueta >Preço: </s.Etiqueta>
-      <s.Inovos id="npreco"></s.Inovos>
+      <s.Inovos id="npreco" maxLength={9}></s.Inovos>
 
       <s.Etiqueta >Qt. Parcelas:</s.Etiqueta>
-      <s.Inovos id="qtd"></s.Inovos>
+      <s.Inovos id="qtd" maxLength={2}></s.Inovos>
 
       <s.Etiqueta >Parcelas Pagas: </s.Etiqueta>
-      <s.Inovos id="pp"></s.Inovos>
+      <s.Inovos id="pp" maxLength={5}></s.Inovos>
 
       <s.Etiqueta >Credor: </s.Etiqueta>
-      <s.Inovos id="credor"></s.Inovos>
+      <s.Inovos id="credor" maxLength={10}></s.Inovos>
 
       <s.Gravar onClick={() => armazenar()}>Salvar</s.Gravar>
 
@@ -81,43 +105,52 @@ export default function TabelaItens() {
         }
 
 
-        {
+                    {
 
-          peca?.map((e, i) => (
-            <>
-              <s.LinhaTr>
+                    peca?.map((e, i) => (
+                      <>
+                        <s.LinhaTr>
 
-                <s.ColunaTd1> {e.npeca} </s.ColunaTd1>
-
-                <s.ColunaTd >
-                  <s.Input value={e.npreco} />
-                </s.ColunaTd>
-
-                <s.ColunaTd >
-                  <s.Input maxLength={2} value={e.qtd} />
-                </s.ColunaTd>
-
-                <s.ColunaTd >
-                  <s.Input maxLength={5} value={e.pp} />
-                </s.ColunaTd>
-
-                <s.ColunaTd >
-                  <s.Input maxLength={10} value={e.credor} />
-                </s.ColunaTd>
+                          <s.ColunaTd1> {e.npeca} </s.ColunaTd1>
 
 
-                <s.ColunaTd>
-                  <s.Botaoexcluir onClick={() => excluir()} />
-                </s.ColunaTd>
 
-              </s.LinhaTr>
-
-            </>
-          )
-          )
+                          <s.ColunaTd >
+                            <s.Input value={e.npreco} />
+                          </s.ColunaTd>
 
 
-        }
+
+
+                          <s.ColunaTd >
+                            <s.Input maxLength={2} value={e.qtd} />
+                          </s.ColunaTd>
+
+
+
+                          <s.ColunaTd >
+                            <s.Input maxLength={5} value={e.pp} />
+                          </s.ColunaTd>
+
+
+
+                          <s.ColunaTd >
+                            <s.Input maxLength={10} value={e.credor} />
+                          </s.ColunaTd>
+
+
+                          <s.ColunaTd>
+                            <s.Botaoexcluir onClick={() => excluir()} />
+                          </s.ColunaTd>
+
+                        </s.LinhaTr>
+
+                      </>
+                    )
+                    )
+
+
+                    }
 
 
       </s.Tabela>
