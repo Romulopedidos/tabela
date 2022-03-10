d55babd45e37e6ce8d7c1918f58bc601e0525972
Romulopedidos <98947494+Romulopedidos@users.noreply.github.com>
2022-03-10 16:16:51 -0300

    ss

--- src/TREINO/treinandomapemcasa.js
+++ src/TREINO/treinandomapemcasa.js
@@ -11,13 +11,13 @@ let objTreino = [{"corda": "Guitarra" , "percussão" : "Bateria" , "tecla" : "pi
 {"aperreio" : sobrinhos , "status" : resultados} 
 ];
 
-let porquin = 0
-function musica(n){
-    porquin += n.acessório
-    console.log(`${n.calcado} tem ${n.cobertor} `)
-    }
+let objEmpresa =  [{ // chave  ---  valor              chave    ----    valor                 chave   ----  valor        chave ---- valor 
+                        "Codigo": 1 ,                        "Nome": "NTDSoft"  ,                "AnoBase":2020   ,       "Endereco":"Av. Engenho Serra Verde, nº 171, Lojas B, C e D",    
+                  "Fone":"(81) 4141-7700",         "Fax":"(81) 3083-3464"  ,                  "NomeSecret":""  ,            "RegSec":"" , 
+                     "Diretor":"",                      "DiretorReg":"",                      "Cidade":"Recife",             "UF":"PE",       
+                     "CEP":"51270-570",                    "DiasLetivos":0,                "MinimaRecupeacao":"",        "CNPJ":"11.611.592/0001-02",
+             "Obs":"CNPJ: 08971817/0001-19" ,      "Email":"ntdsoft@hotmail.com"   ,         "CadastroEscolar":""
+            
+            }]
 
-objTreino.map((n,i)=>
-    musica(n)
-)
 
